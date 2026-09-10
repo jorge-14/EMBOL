67b3ec41b36afe3b2bf1bac47680b5eaa0937cb1
Jorge Luis Choque Callizaya <jchoque@servicios.mc4.com.bo>
2026-09-10 14:36:58 -0400

    Agregar proyecto CMO Embol

---
+++ Embol/back-office-api/src/main/java/backofficeapi/BackOfficeApiApplication.java
@@ -0,0 +1,13 @@
+package backofficeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackOfficeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackOfficeApiApplication.class, args);
+    }
+
+}
---
+++ Embol/back-office-api/src/test/java/backofficeapi/BackOfficeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package backofficeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackOfficeApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
