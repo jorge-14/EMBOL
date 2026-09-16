ce5357e67d8c4f9a45a89956a7df336367f6d3fb
Jorge Luis Choque Callizaya <jchoque@servicios.mc4.com.bo>
2026-09-16 09:22:42 -0400

    Commit

---
+++ Embol/back-office-api/src/main/java/backofficeapi/application/prueba.java
@@ -0,0 +1,15 @@
+package backofficeapi.application;
+
+/*
+ *----------------------------------------
+ *   Código de Aplicación:
+ *   Código de Objeto:
+ *   Descripción:
+ *   Author Prog: Jorge Luis Choque Callizaya
+ *----------------------------------------
+ *   Fecha | Autor | Comentario
+ *   16.09.2026 | Jorge Luis Choque Callizaya | Creación Inicial
+ *----------------------------------------
+ */
+public class prueba {
+}
