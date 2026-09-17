a26fbb9c53debc9da7a372940e2953ea8d66b62d
mroca <mroca@servicios.mc4.com.bo>
2026-09-17 17:33:04 -0400

    Fix> group create new group sin estado de activos/inactivos

--- Embol/web/src/app/features/configuration/user-roles/configs/grupos/grupos-form.config.ts
+++ Embol/web/src/app/features/configuration/user-roles/configs/grupos/grupos-form.config.ts
@@ -5,7 +5,6 @@ export const buildCreateGroupConfig = (): DynamicFormConfig => ({
   fields: [
     { key: 'name', label: 'NOMBRE', type: 'text', placeholder: 'Ej: Operaciones Sur', required: true, colSpan: 2 },
     { key: 'description', label: 'DESCRIPCIÓN', type: 'text', placeholder: 'Describe el propósito del grupo', required: true, colSpan: 2 },
-    { key: 'estado', label: 'ESTADO', type: 'select', options: [{label: 'Activo', value: 'Activo'}, {label: 'Inactivo', value: 'Inactivo'}], required: true, colSpan: 2 }
   ],
   submitLabel: 'Guardar',
   cancelLabel: 'Cancelar'
